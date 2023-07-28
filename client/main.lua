02aff7cb835f83e3c9ca0f7d01460c961c580c4e
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2023-07-28 21:30:11 +0100

    Delete client directory

--- client/main.lua
+++
@@ -1,9 +0,0 @@
-QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('ps_lotery:client:useticket', function (won, price)
-    if won then
-        QBCore.Functions.Notify('You won $' .. price ..'!', 'success', 5000)
-    else
-        QBCore.Functions.Notify('You lost, the new price pot is now $' .. price, 'error', 5000)
-    end
-end)
